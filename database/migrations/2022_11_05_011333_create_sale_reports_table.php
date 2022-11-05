e7f65d3063a6bdbb6484ac1a26fda1d5fe90d98e
Fihri Aziz <fihriaziz@gmail.com>
2022-11-05 08:17:15 +0700

    add migration and model Sale Report

---
+++ database/migrations/2022_11_05_011333_create_sale_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use App\Models\Vehicle;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaleReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Vehicle::class);
+            $table->foreignIdFor(User::class);
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_reports');
+    }
+}
