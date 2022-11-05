0558e3cae85e68a257eb124eaec4f4dfcd368c38
Fihri Aziz <fihriaziz@gmail.com>
2022-11-05 12:47:19 +0700

    create Report service

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,26 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\Report\ReportRepository;
+use App\Service\ReportService;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    protected $reportRepository;
+    protected $reportService;
 
-    public function __construct(ReportRepository $reportRepository)
+    public function __construct(ReportService $reportService)
     {
-        $this->reportRepository = $reportRepository;
+        $this->reportService = $reportService;
     }
 
     public function reportById(Request $req)
     {
-        $report = $this->reportRepository->ReportById($req->id);
+        $report = $this->reportService->ReportById($req->id);
         return $report;
     }
 
     public function reportAll()
     {
-        $reports = $this->reportRepository->Report();
+        $reports = $this->reportService->Report();
         return $reports;
     }
 }
