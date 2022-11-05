166c9a52a9f9934f11ce5fc89e4a844c3d4d07d6
Fihri Aziz <fihriaziz@gmail.com>
2022-11-05 12:57:57 +0700

    declare type

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
@@ -29,7 +31,7 @@ class AuthController extends Controller
 
         return response()->json([
             'access_token' => $token,
-            'token_type' => 'bearer'
+            'token_type' => 'bearer',
         ]);
     }
 }
